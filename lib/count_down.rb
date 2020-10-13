d670a16462711f28163217df95a9c8dfc62b3887
Jacob Marthaller <marthaller.jr@gmail.com>
2020-10-13 18:11:26 +0000

    Automatically backed up by Learn

--- lib/count_down.rb
+++ lib/count_down.rb
@@ -1,1 +1,14 @@
 # Write your code here
+# countdown = 10
+
+# while countdown > 0
+#   puts countdown
+#   countdown -= 1
+#   if countdown == 0
+#   puts "Happy New Year!"
+# end
+# end
+
+while true do 
+  puts "say this forever..."
+end
